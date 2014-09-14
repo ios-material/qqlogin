9ab420341e9bb53307c50d29c5e88a4045b3eb52
Xu Lvcai <xulvcai2008@gmail.com>
2014-09-14 21:43:17 +0800

    init

---
+++ QQLogin/QQAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  QQAppDelegate.h
+//  QQLogin
+//
+//  Created by Reese on 13-6-17.
+//  Copyright (c) 2013年 Reese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class QQViewController;
+
+@interface QQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) QQViewController *viewController;
+
+@end
---
+++ QQLogin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QQLogin
+//
+//  Created by Reese on 13-6-17.
+//  Copyright (c) 2013年 Reese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QQAppDelegate class]));
+    }
+}
